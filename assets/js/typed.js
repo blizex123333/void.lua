b232edd9d9c50b3de93803b09709d0490ca03656
thatscrazybro11 <114077667+blizex123333@users.noreply.github.com>
2023-02-03 17:43:23 +0200

    Update typed.js

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,5 +1,5 @@
 new Typed('#typed',{
-    strings : [,'I only fear allah','رَبَّنَا أَفْرِغْ عَلَيْنَا صَبْراً وَثَبِّتْ أَقْدَامَنَا وَانصُرْنَا عَلَى القَوْمِ الكَافِرِينَ',],
+    strings : [,'I only fear allah','Interior designer, Programmer',],
     typeSpeed : 35,
     delaySpeed : 90,
     showCursor: false,
