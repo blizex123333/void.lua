a3978d60b2a65fbcaf4d6a6d40e975d2f6dc6cae
thatscrazybro11 <114077667+blizex123333@users.noreply.github.com>
2023-02-03 17:42:14 +0200

    Update typed.js

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,5 +1,5 @@
 new Typed('#typed',{
-    strings : [,'I only fear allah','Python programmer','JS programmer.','Lua programmer.','Batch programmer.',],
+    strings : [,'I only fear allah','رَبَّنَا أَفْرِغْ عَلَيْنَا صَبْراً وَثَبِّتْ أَقْدَامَنَا وَانصُرْنَا عَلَى القَوْمِ الكَافِرِينَ',],
     typeSpeed : 35,
     delaySpeed : 90,
     showCursor: false,
