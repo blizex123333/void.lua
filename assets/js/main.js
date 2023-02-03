4907631c5a39acc90e5ac2b163c5e43ae7719c4e
speed <76481053+chinged@users.noreply.github.com>
2023-02-03 15:36:41 +0000

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+console.log("Speed#9999 // sixtyseven.lol LLC All rights reserved.");
+
+function audioPlay() {
+  var audio = document.getElementById("audio");
+  audio.volume = 0.4;
+  audio.play()
+}
+
+function videoPlay() {
+  var video = document.getElementById("video");
+  video.play()
+}
---
+++ assets/js/typed.js
@@ -0,0 +1,9 @@
+new Typed('#typed',{
+    strings : [,'I only fear allah','Python programmer','JS programmer.','Lua programmer.','Batch programmer.',],
+    typeSpeed : 35,
+    delaySpeed : 90,
+    showCursor: false,
+    loop : true
+})
+
+
